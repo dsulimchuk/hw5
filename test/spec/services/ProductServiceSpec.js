26ce2a03939fec713c56157da7cb1bcce60accbc
ds <dserge.spb@gmail.com>
2014-12-28 09:23:17 +0300

    fix tests after refactoring

--- test/spec/services/ProductServiceSpec.js
+++ test/spec/services/ProductServiceSpec.js
@@ -1,23 +1,26 @@
 'use strict';
 describe('Service: ProductService', function(){
-    var fakeRespond = [{
-        'id'          : 3,
-        'title'       : 'Item 3',
-        'thumb'       : '03.jpg',
-        'description' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore adipiscing elit. Ut enim.',
-        'timeleft'    : 2,
-        'watchers'    : 12,
-        'price'       : 39
-    },
-        {
-            'id'          : 4,
-            'title'       : 'Item 4',
-            'thumb'       : '04.jpg',
-            'description' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore adipiscing elit. Ut enim.',
-            'timeleft'    : 2,
-            'watchers'    : 12,
-            'price'       : 72
-        }];
+    var fakeRespond = {
+        "items":[
+                {
+                    'id'          : 3,
+                    'title'       : 'Item 3',
+                    'thumb'       : '03.jpg',
+                    'description' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore adipiscing elit. Ut enim.',
+                    'timeleft'    : 2,
+                    'watchers'    : 12,
+                    'price'       : 39
+                },
+                {
+                    'id'          : 4,
+                    'title'       : 'Item 4',
+                    'thumb'       : '04.jpg',
+                    'description' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore adipiscing elit. Ut enim.',
+                    'timeleft'    : 2,
+                    'watchers'    : 12,
+                    'price'       : 72
+                }]
+    };
     var srv,
         httpB;
 
@@ -28,17 +31,17 @@ describe('Service: ProductService', function(){
         srv = ProductService;
         httpB =_$httpBackend_;
         //for getProduct
-        httpB.whenGET(/data\/products(\.json)?$/i).respond(fakeRespond);
+        httpB.whenGET(/.*\/product\/featured$/i).respond(fakeRespond);
         //for find
-        httpB.whenGET(/data\/products(\.json)?\?/i).respond(fakeRespond.concat(fakeRespond));
+        httpB.whenGET(/.*\/product\/search\?/i).respond({'items':fakeRespond.items.concat(fakeRespond.items)});
         //for getProduct
-        httpB.whenGET(/data\/products\/\d+(\.json)/i).respond(_.rest(fakeRespond));
+        httpB.whenGET(/.*\/product\/\d+/i).respond(_.rest(fakeRespond.items));
 
 
     }));
 
     it('getProducts method should return array', function(){
-        httpB.expectGET(/data\/products(\.json)?$/i);
+        httpB.expectGET(/.*\/product\/featured?$/i);
         srv.getProducts().then(function(result){
             expect(result.length).toBe(2);
         });
@@ -46,7 +49,7 @@ describe('Service: ProductService', function(){
     });
 
     it('find method should return array', function(){
-        httpB.expectGET(/data\/products(\.json)?\?/i);
+        httpB.expectGET(/.*\/product\/search\?/i);
         srv.find({ggg: 222}).then(function(result){
             expect(result.length).toBe(4);
         });
@@ -54,9 +57,10 @@ describe('Service: ProductService', function(){
     });
 
     it('getProductById method should return one element', function(){
-        httpB.expectGET(/data\/products\/\d+(\.json)/i);
+        httpB.expectGET(/.*\/product\/\d+/i);
         srv.getProductById(222).then(function(result){
             expect(result.length).toBe(1);
+            expect(result[0].id).toBe(4);
         });
         httpB.flush();
     });
